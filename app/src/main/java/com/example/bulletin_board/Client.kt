08b50a0dfbb5b56cf53b85416b62eeb71b0d9f68
BearGom2 <31925642+sjcalaa@users.noreply.github.com>
2021-05-18 10:42:48 +0900

    도메인과 연결 설정

--- app/src/main/java/com/example/bulletin_board/Client.kt
+++ app/src/main/java/com/example/bulletin_board/Client.kt
@@ -7,14 +7,14 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Client {
-    val BASE_URL: String = "http://49.50.166.23:3000/" //서버 주소
+    private const val BASE_URL: String = "http://beargom2.site/" //서버 주소
     val getClient: API
         get() {
             val gson = GsonBuilder()
                 .setLenient()
                 .create()
             val interceptor = HttpLoggingInterceptor()
-            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
             val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
             val retrofit = Retrofit.Builder()//retrofit 선언
